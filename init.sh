3a97a9c5751faf1f08bdd2185c79a8c4b01288fe
niwa-s <souta.niwa.prog@gmail.com>
2022-06-03 18:47:41 +0900

    fix slow-queryの有効化

--- init.sh
+++ init.sh
@@ -23,6 +23,5 @@ rm alp
 # nginx.confにログフォーマットを追加
 sudo cp "${SCRIPT_DIR}/nginx_log_format.conf" /etc/nginx/sites-enabled/.
 
-# $HOME/.my.cnfにログフォーマットを追加
-sudo cp "${SCRIPT_DIR}/.my.cnf" "${HOME}/."
-sudo cat "${SCRIPT_DIR}/.my.cnf" >> /etc/mysql/mysql.conf.d/mysqld.cnf 
+# mysql.cnfの末尾に、slow-queryの設定を追加
+sudo sh -c "cat ${SCRIPT_DIR}/.my.cnf >> /etc/mysql/mysql.conf.d/mysqld.cnf"
